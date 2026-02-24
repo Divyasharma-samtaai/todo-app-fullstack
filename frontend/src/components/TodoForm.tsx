ffacee66001833ff5f23bdac6af8b94cd7080108
divyaaa-sharma <rdsharma46755@gmail.com>
2026-02-24 17:10:22 +0530

    feat: implement add todo functionality

---
+++ frontend/src/components/TodoForm.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+interface TodoFormProps {
+  onAdd: (title: string) => void;
+}
+
+export default function TodoForm({ onAdd }: TodoFormProps) {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (title.trim() === "") return;
+    onAdd(title);
+    setTitle("");
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter todo title"
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </>
+  );
+}
