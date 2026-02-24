3a09d1ef8423a1f9ee0745f0382cbb60033a4a84
divyaaa-sharma <rdsharma46755@gmail.com>
2026-02-24 15:12:47 +0530

    feat: fetch todos from backend on component mount

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import type { Todo } from "./types/Todo";
+import { api } from "./services/Api";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const todos = await api.getTodos();
+        setTodos(todos);
+      } catch (error) {
+        console.error("Error fetching todos:", error);
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <h1>Todo Count: {todos.length}</h1>
+    </div>
+  );
+};
 
 export default App;
